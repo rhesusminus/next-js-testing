e637c4bd528eabdb3dc496d5c3f29ddbc8b81ef0
Erik Niilivirta <erik.niilivirta@gmail.com>
2020-06-07 12:45:21 +0300

    asodjkapdsoj

--- lib/mutations.tsx
+++ lib/mutations.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-const instance = axios.create({ baseURL: 'localhost:3000/api' })
+const instance = axios.create({ baseURL: 'https://my-first-app.rhesusminus.now.sh/api' })
 
 export const login = (credentials) => instance.post(`login`, { ...credentials })
